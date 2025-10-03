55f72db9a12b57c3d8fad7dfabaa61fec278d1a5
an24-bit <gulf947@googlemail.com>
2025-10-03 01:33:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -65,5 +65,5 @@ app.get("/", (req, res) => {
 // ---- Start Server ----
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
